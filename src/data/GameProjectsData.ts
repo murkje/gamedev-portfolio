52b48d7f363fa3920a0ad12ff8b7c64f9d0be91c
murkje <markhoitsma@gmail.com>
2024-11-03 23:46:37 -0800

    Update GameProjectsData.ts

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -148,8 +148,9 @@ export default [
     </div>
 
     <div class="paragraph center">
-        <img class="pc-screenshot" src="https://img.itch.zone/aW1hZ2UvMzE1NTQ3LzE2NzU4MDIucG5n/347x500/hVUFdm.png/" alt="Skirmish Screenshot" />
-        <img class="pc-screenshot" src="https://img.itch.zone/aW1hZ2UvMzE1NTQ3LzE2NzU4MDAucG5n/347x500/Ntholc.png/" alt="Skirmish Screenshot" />
-        <img class="pc-screenshot" src="https://img.itch.zone/aW1hZ2UvMzE1NTQ3LzE2NzYwNzMucG5n/347x500/5Xgr3G.png/" alt="Skirmish Screenshot" />
+        <img class="pc-screenshot" <a href="https://imgbb.com/"><img src="https://i.ibb.co/N7MYqph/hVUFdm.png" alt="hVUFdm" border="0"></a> />
+        <img class="pc-screenshot" <a href="https://imgbb.com/"><img src="https://i.ibb.co/6nC8Z9m/Ntholc.png" alt="Ntholc" border="0"></a> />
+        <img class="pc-screenshot" <a href="https://imgbb.com/"><img src="https://i.ibb.co/ZVpr1Vj/5Xgr3G.png" alt="5Xgr3G" border="0"></a>
+        <img class="pc-screenshot" <a href="https://imgbb.com/"><img src="https://i.ibb.co/K5fQmGh/aQf6wA.png" alt="aQf6wA" border="0"></a>
     </div>`, "#e48246")
 ];
