d242f2795c38a2c27b8bb0e7005a88e9c4b74dfe
murkje <markhoitsma@gmail.com>
2024-11-04 00:54:24 -0800

    Update GameProjectsData.ts

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -60,7 +60,7 @@ export default [
 
     <div class="paragraph center">
         <img class="pc-screenshot" src="https://i.ibb.co/Wxv1JtF/image-2024-11-04-002345596.png" alt="image-2024-11-04-002345596" border="0">
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
+        <img class="pc-screenshot" src="https://i.ibb.co/fHVb7xh/image-2024-11-04-005338508.png" alt="image-2024-11-04-005338508" border="0">
         <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
         <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
     </div>
