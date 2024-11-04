95c7b31b9de16d21cd8b5acb9062f1e0af431922
murkje <markhoitsma@gmail.com>
2024-11-03 23:53:47 -0800

    Update GameProjectsData.ts

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -4,23 +4,25 @@ export default [
     new ProjectData("project-1", "Journey To Ardunia", "img/projects/project-1-icon.png", 
     `
     <div class="paragraph">
-     <strong>Journey To Ardunia</strong> is a thriller game made in Unity.
-     <br/>Image by <a href="https://www.pexels.com/fr-fr/@knownasovan" target="_blank">OVAN</a>.
+        <strong>Journey To Ardunia</strong> is a thriller game made in Unity.
+        <br/>Image by <a href="https://www.pexels.com/fr-fr/@knownasovan" target="_blank">OVAN</a>.
     </div>
     <div class="paragraph center">
         <iframe class="youtube" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
     </div>
     <div class="paragraph center">
-        <a href="https://play.google.com/store/apps/details?id=some.playstore.thing" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
+        <a href="https://play.google.com/store/apps/details?id=some.playstore.thing" target="_blank">
+            <img src="img/projects/play-store-logo.png" alt="Play Store badge" />
+        </a>
     </div>
 
     <div class="paragraph">
         Main features :
         <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
+            <li>Some stuff</li>
+            <li>Some great stuff</li>
+            <li>More awesome stuff</li>
+            <li>And then some</li>
         </ul>
     </div>
 
@@ -29,9 +31,10 @@ export default [
         <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
     </div>
     `, "#23bd69", true),
+    
     new ProjectData("project-2", "Nyx:Across Worlds", "img/projects/project-2-icon.png", `
     <div class="paragraph">
-        <strong>Nyx:Across Worlds</strong>Upcoming Visual Novel I'm making with a friend in Germany!
+        <strong>Nyx:Across Worlds</strong> Upcoming Visual Novel I'm making with a friend in Germany!
         <br/>Concept art by <a target="_blank" href="https://www.instagram.com/zanrisuy/?locale=nl&hl=af">Zanrisuy</a>.
     </div>
     <div class="paragraph center">
@@ -41,30 +44,31 @@ export default [
     <div class="paragraph">
         Main features :
         <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
+            <li>Some stuff</li>
+            <li>Some great stuff</li>
+            <li>More awesome stuff</li>
+            <li>And then some</li>
         </ul>
     </div>
 
     <div class="paragraph">
-    <div class="notice">
-        Windows build available on <a href="https://some.where/nice" target="_blank">itch.io</a>.
-        Source code is available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
-    </div>
+        <div class="notice">
+            Windows build available on <a href="https://some.where/nice" target="_blank">itch.io</a>.
+            Source code is available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
+        </div>
     </div>
 
     <div class="paragraph center">
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
+        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
+        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
+        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
+        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
     </div>
     `, "#5a78af"),
+    
     new ProjectData("project-3", "Ecocraft", "img/projects/project-3-icon.png", `
     <div class="paragraph">
-        <strong>Drawing Overload</strong>Serious game made for the Global Goals jam at school.
+        <strong>Drawing Overload</strong> Serious game made for the Global Goals jam at school.
         <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@miphotography">Miesha Maiden</a>.
     </div>
     <div class="paragraph center">
@@ -74,46 +78,47 @@ export default [
     <div class="paragraph">
         Main features :
         <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
+            <li>Some stuff</li>
+            <li>Some great stuff</li>
+            <li>More awesome stuff</li>
+            <li>And then some</li>
         </ul>
     </div>
 
     <div class="paragraph">
         <div class="notice">
-        Playable in the browser (WebGL) on <a href="https://some.where/nice" target="_blank">itch.io</a>.
-        Source code is available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
+            Playable in the browser (WebGL) on <a href="https://some.where/nice" target="_blank">itch.io</a>.
+            Source code is available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
         </div>
     </div>
 
     <div class="paragraph center">
-        <img class="pc-screenshot" <a href="https://ibb.co/88y1kkG"><img src="https://i.ibb.co/gg0kYYQ/534x530.png" alt="534x530" border="0"></a> />
-        <img class="pc-screenshot" <a href="https://ibb.co/hZW9BdR"><img src="https://i.ibb.co/Hxz4tPD/ccgs.png" alt="ccgs" border="0"></a> />
-        <img class="pc-screenshot" <a href="https://ibb.co/gwTxNGN"><img src="https://i.ibb.co/wCWfVTV/qad.png" alt="qad" border="0"></a> />
-        <img class="pc-screenshot" <a href="https://ibb.co/LY6BF9D"><img src="https://i.ibb.co/wBC8kpZ/iss.png" alt="iss" border="0"></a><br /><a target='_blank'</a><br /> />
+        <img class="pc-screenshot" src="https://i.ibb.co/gg0kYYQ/534x530.png" alt="534x530 Screenshot" border="0" />
+        <img class="pc-screenshot" src="https://i.ibb.co/Hxz4tPD/ccgs.png" alt="ccgs Screenshot" border="0" />
+        <img class="pc-screenshot" src="https://i.ibb.co/wCWfVTV/qad.png" alt="qad Screenshot" border="0" />
+        <img class="pc-screenshot" src="https://i.ibb.co/wBC8kpZ/iss.png" alt="iss Screenshot" border="0" />
     </div>
     `, "#383838"),
+    
     new ProjectData("project-4", "Let 'em Cook!", "img/projects/project-4-icon.png", `
     <div class="paragraph">
-    <strong>Eugeneable</strong>Upcoming game inspired by games like Overcooked and Plate-Up!
-    <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@neo8iam">NEOSiAM 2020</a>.
+        <strong>Eugeneable</strong> Upcoming game inspired by games like Overcooked and Plate-Up!
+        <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@neo8iam">NEOSiAM 2020</a>.
     </div>
     
     <div class="paragraph">
         Main features :
         <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
+            <li>Some stuff</li>
+            <li>Some great stuff</li>
+            <li>More awesome stuff</li>
+            <li>And then some</li>
         </ul>
     </div>
 
     <div class="paragraph">
         <div class="notice">
-        Windows build available on <a href="https://some.where/nice" target="_blank">itch.io</a>.
+            Windows build available on <a href="https://some.where/nice" target="_blank">itch.io</a>.
         </div>
     </div>
 
@@ -122,9 +127,10 @@ export default [
         <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Eugeneable Screenshot" />
     </div>
     `, "#e80fb7"),
+    
     new ProjectData("project-5", "Skirmish", "img/projects/project-5-icon.png", `
     <div class="paragraph">
-        <strong>Cloud Drew Land</strong>One button game made by Extra Nice. I was a junior developer & level designer.
+        <strong>Cloud Drew Land</strong> One button game made by Extra Nice. I was a junior developer & level designer.
         <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@cottonbro">cottonbro</a>.
     </div>
     <div class="paragraph center">
@@ -133,32 +139,23 @@ export default [
     <div class="paragraph">
         Main features :
         <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
+            <li>Some stuff</li>
+            <li>Some great stuff</li>
+            <li>More awesome stuff</li>
+            <li>And then some</li>
         </ul>
     </div>
 
     <div class="paragraph">
         <div class="notice">
-        Windows build available on <a href="https://some.where/nice" target="_blank">itch.io</a>.
-        Source code available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
+            Windows build available on <a href="https://some.where/nice" target="_blank">itch.io</a>.
+            Source code available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
         </div>
     </div>
 
     <div class="paragraph center">
-    <a href="https://imgbb.com/">
-        <img class="pc-screenshot" src="https://i.ibb.co/N7MYqph/hVUFdm.png" alt="hVUFdm" border="0" />
-    </a>
-    <a href="https://imgbb.com/">
-        <img class="pc-screenshot" src="https://i.ibb.co/6nC8Z9m/Ntholc.png" alt="Ntholc" border="0" />
-    </a>
-    <a href="https://imgbb.com/">
-        <img class="pc-screenshot" src="https://i.ibb.co/ZVpr1Vj/5Xgr3G.png" alt="5Xgr3G" border="0" />
-    </a>
-    <a href="https://imgbb.com/">
-        <img class="pc-screenshot" src="https://i.ibb.co/K5fQmGh/aQf6wA.png" alt="aQf6wA" border="0" />
-    </a>
-</div>
+        <img class="pc-screenshot" src="https://i.ibb.co/N7MYqph/hVUFdm.png" alt="hVUFdm Screenshot" border="0" />
+        <img class="pc-screenshot" src="https://i.ibb.co/6nC8Z9m/Ntholc.png" alt="Ntholc Screenshot" border="0" />
+        <img class="pc-screenshot" src="https://i.ibb.co/ZVpr1Vj/5Xgr3G.png" alt="5Xgr3G Screenshot" border="0" />
+    </div>`, "#e48246")
 ];
